164b0629abfe3e753cf728e4a8cca92b24421805
Adam Hundley <adamhundley@gmail.com>
2016-02-19 13:36:39 -0700

    first commit

--- app/models/fan.rb
+++ app/models/fan.rb
@@ -1,2 +1,18 @@
 class Fan < ActiveRecord::Base
+  validates :name,  presence: true,
+                    format: {with: /\A[a-zA-Z]+\z/, message: "only allows uppercase and lowercase letters"}
+
+  validates :email, presence: true,
+                    uniqueness: true,
+                    length: {in: 5..50}
+
+  validate :no_dicks
+
+  belongs_to :location 
+
+  def no_dicks
+    errors.add(:name, "cannot be Dick") if name == "Dick"
+  end
+
+  has_many :donations
 end
--- test/models/fan_test.rb
+++ test/models/fan_test.rb
@@ -1,7 +1,61 @@
 require 'test_helper'
 
 class FanTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should belong_to(:location)
+
+  def valid_attributes
+    {
+      name: "Jorge",
+      email: "yosoybelieber@example.com"
+    }
+  end
+
+  def test_it_creates_a_fan
+    result = Fan.new(valid_attributes)
+
+    assert result.valid?
+    assert_equal "Jorge", result.name
+    assert_equal "yosoybelieber@example.com", result.email
+  end
+
+  def test_it_cannot_create_a_fan_with_a_name
+    result = Fan.new(email: "yosoybelieber@example.com")
+
+    assert result.invalid?
+  end
+
+  def test_it_cannot_create_a_fan_with_the_same_email
+    2.times {Fan.create(valid_attributes)}
+
+    result = Fan.where(email:  "yosoybelieber@example.com")
+    assert_equal 1, result.count
+  end
+
+  def test_it_only_accepts_letters_as_a_name
+    fan = Fan.new(
+      name: "Jorge1",
+      email: "yosoybelieber@example.com"
+      )
+
+      refute fan.valid?
+  end
+
+  def test_it_only_accepts_an_email_between_5_and_50_characters
+    fan = Fan.new(
+      name: "Jorge1",
+      email: "Jorg"
+      )
+
+    assert fan.invalid?
+  end
+
+  def test_it_cannot_create_a_fan_named_dick
+    fan = Fan.new(
+      name: "Dick",
+      email: "dick@dick.com"
+      )
+
+    assert fan.invalid?
+    assert_includes fan.errors.full_messages, "Name cannot be Dick"
+  end
 end
